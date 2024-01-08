e829cde31a16c990134a6fb6cd9072e9031f0795
vinhanhtth <51294794+vinhanhtth@users.noreply.github.com>
2024-01-09 06:20:57 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express =require("express");
+const cors = require("cors");
+const ApiError = require("./app/api-error");
+
+const app = express();
+const contactsRouter = require("./app/routes/contact.route");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+app.get("/",(req,res) => {
+        res.json({ message:"Welcome to contact book application"});
+});
+// handle 404 response
+app.use((req, res, next) => {
+        return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+        return res.status(error.statusCode || 500).json({
+                message: error.message || "Internal Server Error",
+        });
+});
+module.exports = app;
